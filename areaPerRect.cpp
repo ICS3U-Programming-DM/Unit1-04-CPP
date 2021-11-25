a8496d63722c33c183112f15d104d1ed3fc1cdfe
Ubuntu <ubuntu@ip-172-31-17-200.ec2.internal>
2021-11-25 20:03:57 +0000

    created areaPerRect

---
+++ areaPerRect.cpp
@@ -0,0 +1,15 @@
+// Copyright © 2021 Dylan Melo All rights reserved
+//
+// Created by Dylan Melo
+// Created on November 2021
+// Calculates area and perimiter of rectangle
+
+#include <iostream>
+
+int main() {
+  std::cout << "For a rectangle that has dimensions\n";
+  std::cout << "length 5cm and width 9cm:\n";
+  std::cout << "\n";
+  std::cout << "Area = " << 5 * 9 << " cm^2\n";
+  std::cout << "Perimeter = " << 2* (5 + 9) << " cm\n";
+}
---
+++ basicMath.cpp
@@ -0,0 +1,17 @@
+// Copyright © 2021 Dylan Melo All rights reserved
+//
+// Created by Dylan Melo
+// Created on November 2021
+// this program does basic math
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+  std::cout << "9 + 2 = " << 9+2 << std::endl;
+  std::cout << "7 - 3 = " << 7-3 << std::endl;
+  std::cout << "10 / 2 = " << 10/2 << std::endl;
+  std::cout << "8 - 4 / 2 = " << 8-4/2 << std::endl;
+  std::cout << "2^3 = " << pow(2, 3) << std::endl;
+  std::cout << "1 / 3 = " << float(1)/float(3) << std::endl;
+}
